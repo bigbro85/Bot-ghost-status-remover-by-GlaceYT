66f2bb917eec4db6ab0e6257a546c4d4b47713c9
bigbro85 <141200881+bigbro85@users.noreply.github.com>
2026-06-18 14:55:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching over FCSO"];
-const statusTypes = [ 'idl'];
+const statusMessages = ["Watching over WSRPC"];
+const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
